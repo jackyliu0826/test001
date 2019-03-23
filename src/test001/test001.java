9bc8afd6887ddef6c5266dea4c52d4ee1da7015e
Jie <Jie@DESKTOP-6LFCUNE.uconnect.utah.edu>
2019-03-23 15:27:31 -0600

    finished test upload

--- src/test001/test001.java
+++ src/test001/test001.java
@@ -5,5 +5,9 @@ public class test001 {
 	public test001() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	}
 
 }
